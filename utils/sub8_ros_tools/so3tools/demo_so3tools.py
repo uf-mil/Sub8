37cde39b59912af277434382c4dbe41011d3fbfc
Jason Nezvadovitz <nez.jason7@gmail.com>
2015-11-06 04:55:02 -0500

    SO3TOOLS: conform to PEP8
    - Needed a space after every comma no matter what
    - A couple other semantic changes
    - Jake is shorter than Mike Seese

--- utils/sub8_ros_tools/so3tools/demo_so3tools.py
+++ utils/sub8_ros_tools/so3tools/demo_so3tools.py
@@ -26,9 +26,9 @@
 
 # standard
 from __future__ import division
-import time
 # 3rd party
-import numpy as np, numpy.linalg as npl
+import numpy as np
+import numpy.linalg as npl
 import transformations as trns
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -41,12 +41,12 @@ import so3tools as so3
 # Define time:
 dt = 0.01  # global time step (s)
 T = 10  # simulation duration (s)
-t_arr = np.arange(0,T,dt)  # time values array (s)
+t_arr = np.arange(0, T, dt)  # time values array (s)
 
 # Define state and initial conditions:
-q = [0.5,0.5,0.5,0.5]  # orientation state quaternion representing a conversion ***from body frame to world frame***
-q = trns.unit_vector(trns.random_vector(4)-0.5)  # actually let's start somewhere random instead
-w = np.array([10,4,-2])  # angular velocity state (rad/s) in world frame
+q = [0.5, 0.5, 0.5, 0.5]  # orientation state quaternion representing a conversion ***from body frame to world frame***
+q = trns.unit_vector(trns.random_vector(4) - 0.5)  # actually let's start somewhere random instead
+w = np.array([10, 4, -2])  # angular velocity state (rad/s) in world frame
 print('Initial Orientation: {}'.format(q))
 print('Initial Ang. Velocity: {}'.format(w))
 
@@ -54,14 +54,14 @@ print('Initial Ang. Velocity: {}'.format(w))
 I = np.array([[ 1   , 0.1  , 0.02],
               [0.01 ,  2   , 0.03],
               [0.02 , 0.03 ,  3 ]])  # inertia matrix in body frame (kg*m^2)
-# I = np.diag([1,2,3])  # cool to compare to diagonal inertia matrix case (which would imply body frame is principal frame)
+# I = np.diag([1, 2, 3])  # cool to compare to diagonal inertia matrix case (which would imply body frame is principal frame)
 invI = npl.inv(I)  # store inverse for future use
 
 # Controller setup (set gains to 0 if you want to test free spin):
-q_des = [1,0,0,0]  # desired orientation state (need not be identity)
-w_des = [0,0,0]  # desired angular velocity state (need not be zero)
-kp = [140,140,140]  # proportional gain (roll, pitch, yaw)
-kd = [170,170,170]  # derivative gain (roll, pitch, yaw)
+q_des = [1, 0, 0, 0]  # desired orientation state (need not be identity)
+w_des = [0, 0, 0]  # desired angular velocity state (need not be zero)
+kp = [140, 140, 140]  # proportional gain (roll, pitch, yaw)
+kd = [170, 170, 170]  # derivative gain (roll, pitch, yaw)
 print('Desired Orientation: {}'.format(q_des))
 print('Desired Ang. Velocity: {}'.format(w_des))
 print('Proportional Gains: {}'.format(kp))
@@ -73,13 +73,13 @@ framerate = 20  # The current MatPlotLib implementation is slow and messed up- a
 
 # Visually represent the body as three perpendicular lines, each defined by its endpoints:
 linlen = 0.5
-xline = np.array([[0,linlen],[0,0],[0,0]])
-yline = np.array([[0,0],[0,linlen],[0,0]])
-zline = np.array([[0,0],[0,0],[0,linlen]])
+xline = np.array([[0, linlen], [0, 0], [0, 0]])
+yline = np.array([[0, 0], [0, linlen], [0, 0]])
+zline = np.array([[0, 0], [0, 0], [0, linlen]])
 body = np.concatenate((xline, yline, zline), axis=1)  # each column is a point in body
 
 # Initialize histories for recording the simulation:
-q_history = np.zeros((len(t_arr),4))
+q_history = np.zeros((len(t_arr), 4))
 roll_history, pitch_history, yaw_history = np.zeros(len(t_arr)), np.zeros(len(t_arr)), np.zeros(len(t_arr))  # for plotting something understandable
 body_world_history = np.zeros((body.shape[0], body.shape[1], len(t_arr)))  # will store all the body points expressed in world frame at each instant
 
@@ -130,23 +130,23 @@ body_world_history = np.zeros((body.shape[0], body.shape[1], len(t_arr)))  # wil
 # accurate RK4 implementation of the angle axis method could and should be implemented.
 # ---
 # Simulate over t_arr:
-for i,t in enumerate(t_arr):
+for i, t in enumerate(t_arr):
     # Record current state:
-    q_history[i,:] = np.copy(q)
-    roll_history[i], pitch_history[i], yaw_history[i] = trns.euler_from_quaternion(q,'rxyz')
-    body_world_history[:,:,i] = so3.apply(q, body)
+    q_history[i, :] = np.copy(q)
+    roll_history[i], pitch_history[i], yaw_history[i] = trns.euler_from_quaternion(q, 'rxyz')
+    body_world_history[:, :, i] = so3.apply(q, body)
     # Current values needed to compute next state:
     I_world = so3.apply(q, so3.apply(q, I).T)  # current inertia matrix in world frame noting I = I.T
     H = I_world.dot(w)  # current angular momentum
-    dq = so3.quaternion_from_axle(w*dt)  # change in orientation for this timestep instant
+    dq = so3.quaternion_from_axle(w * dt)  # change in orientation for this timestep instant
     # PD controller... in ONLY THREE LINES BITCHES:
     q_err = so3.error(q, q_des)  # q_err is an axle
     w_err = w_des - w
-    torque = kp*q_err + kd*w_err
+    torque = (kp * q_err) + (kd * w_err)
     # Compute next state:
     q = so3.plus(dq, q)  # new orientation computed using dq and old q
     I_world = so3.apply(q, so3.apply(q, I).T)  # new I_world computed using new q
-    H = H + torque*dt  # new H from old H and torque
+    H = H + (torque * dt)  # new H from old H and torque
     w = npl.inv(I_world).dot(H)  # new angular velocity computed using new I_world and new H
 
 ################################################# DISPLAY
@@ -154,43 +154,43 @@ for i,t in enumerate(t_arr):
 fig = plt.figure()
 fig.suptitle('Orientation State Evolution', fontsize=24)
 
-if animate == True:
+if animate:
     ax4 = p3.Axes3D(fig)
-    ax4.set_xlim3d([-1,1])
-    ax4.set_ylim3d([-1,1])
-    ax4.set_zlim3d([-1,1])
+    ax4.set_xlim3d([-1, 1])
+    ax4.set_ylim3d([-1, 1])
+    ax4.set_zlim3d([-1, 1])
     ax4.set_xlabel('- World X +')
     ax4.set_ylabel('- World Y +')
     ax4.set_zlabel('- World Z +')
     ax4.grid(True)
 
     body_des = so3.apply(q_des, body)
-    ax4.plot(2*body_des[0,:2], body_des[1,:2], body_des[2,:2], color='red', ls='--', linewidth=0.8)
-    ax4.plot(body_des[0,2:4], 2*body_des[1,2:4], body_des[2,2:4], color='green', ls='--', linewidth=0.8)
-    ax4.plot(body_des[0,4:6], body_des[1,4:6], 2*body_des[2,4:6], color='blue', ls='--', linewidth=0.8)
+    ax4.plot(2 * body_des[0, :2], body_des[1, :2], body_des[2, :2], color='red', ls='--', linewidth=0.8)
+    ax4.plot(body_des[0, 2:4], 2 * body_des[1, 2:4], body_des[2, 2:4], color='green', ls='--', linewidth=0.8)
+    ax4.plot(body_des[0, 4:6], body_des[1, 4:6], 2 * body_des[2, 4:6], color='blue', ls='--', linewidth=0.8)
 
-    ax4.plot(2*body_world_history[0,:2,0], body_world_history[1,:2,0], body_world_history[2,:2,0], color='red', ls=':', linewidth=0.8)
-    ax4.plot(body_world_history[0,2:4,0], 2*body_world_history[1,2:4,0], body_world_history[2,2:4,0], color='green', ls=':', linewidth=0.8)
-    ax4.plot(body_world_history[0,4:6,0], body_world_history[1,4:6,0], 2*body_world_history[2,4:6,0], color='blue', ls=':', linewidth=0.8)
+    ax4.plot(2 * body_world_history[0, :2, 0], body_world_history[1, :2, 0], body_world_history[2, :2, 0], color='red', ls=':', linewidth=0.8)
+    ax4.plot(body_world_history[0, 2:4, 0], 2 * body_world_history[1, 2:4, 0], body_world_history[2, 2:4, 0], color='green', ls=':', linewidth=0.8)
+    ax4.plot(body_world_history[0, 4:6, 0], body_world_history[1, 4:6, 0], 2 * body_world_history[2, 4:6, 0], color='blue', ls=':', linewidth=0.8)
 
-    x = ax4.plot(body_world_history[0,:2,0], body_world_history[1,:2,0], body_world_history[2,:2,0], color='red', linewidth=4)
-    y = ax4.plot(body_world_history[0,2:4,0], body_world_history[1,2:4,0], body_world_history[2,2:4,0], color='green', linewidth=4)
-    z = ax4.plot(body_world_history[0,4:6,0], body_world_history[1,4:6,0], body_world_history[2,4:6,0], color='blue', linewidth=4)
+    x = ax4.plot(body_world_history[0, :2, 0], body_world_history[1, :2, 0], body_world_history[2, :2, 0], color='red', linewidth=4)
+    y = ax4.plot(body_world_history[0, 2:4, 0], body_world_history[1, 2:4, 0], body_world_history[2, 2:4, 0], color='green', linewidth=4)
+    z = ax4.plot(body_world_history[0, 4:6, 0], body_world_history[1, 4:6, 0], body_world_history[2, 4:6, 0], color='blue', linewidth=4)
 
     def update(arg, ii=[0]):
         i = ii[0]
         if np.isclose(t_arr[i], np.around(t_arr[i], 1)):
             fig.suptitle('Orientation State Evolution (Time: {})'.format(t_arr[i]), fontsize=24)
-        x[0].set_data(body_world_history[0,:2,i], body_world_history[1,:2,i])
-        x[0].set_3d_properties(body_world_history[2,:2,i])
-        y[0].set_data(body_world_history[0,2:4,i], body_world_history[1,2:4,i])
-        y[0].set_3d_properties(body_world_history[2,2:4,i])
-        z[0].set_data(body_world_history[0,4:6,i], body_world_history[1,4:6,i])
-        z[0].set_3d_properties(body_world_history[2,4:6,i])
-        ii[0] += int(1/(dt*framerate))
+        x[0].set_data(body_world_history[0, :2, i], body_world_history[1, :2, i])
+        x[0].set_3d_properties(body_world_history[2, :2, i])
+        y[0].set_data(body_world_history[0, 2:4, i], body_world_history[1, 2:4, i])
+        y[0].set_3d_properties(body_world_history[2, 2:4, i])
+        z[0].set_data(body_world_history[0, 4:6, i], body_world_history[1, 4:6, i])
+        z[0].set_3d_properties(body_world_history[2, 4:6, i])
+        ii[0] += int(1 / (dt * framerate))
         if ii[0] >= len(t_arr):
             ii[0] = 0
-        return [x,y,z]
+        return [x, y, z]
 
     ani = animation.FuncAnimation(fig, func=update, interval=dt*1000)
     print('Remember to keep the diplay window aspect ratio square!')
@@ -199,20 +199,20 @@ if animate == True:
 
 else:
     # Plot roll:
-    ax1 = fig.add_subplot(3,1,1)
-    ax1.plot(t_arr, roll_history*180/np.pi)
+    ax1 = fig.add_subplot(3, 1, 1)
+    ax1.plot(t_arr, roll_history * 180 / np.pi)
     ax1.set_ylabel('roll (deg)', fontsize=16)
     ax1.grid(True)
 
     # Plot pitch:
-    ax2 = fig.add_subplot(3,1,2)
-    ax2.plot(t_arr, pitch_history*180/np.pi)
+    ax2 = fig.add_subplot(3, 1, 2)
+    ax2.plot(t_arr, pitch_history * 180 / np.pi)
     ax2.set_ylabel('pitch (deg)', fontsize=16)
     ax2.grid(True)
 
     # Plot yaw:
-    ax3 = fig.add_subplot(3,1,3)
-    ax3.plot(t_arr, yaw_history*180/np.pi)
+    ax3 = fig.add_subplot(3, 1, 3)
+    ax3.plot(t_arr, yaw_history * 180 / np.pi)
     ax3.set_xlabel('time (s)', fontsize=16)
     ax3.set_ylabel('yaw (deg)', fontsize=16)
     ax3.grid(True)
