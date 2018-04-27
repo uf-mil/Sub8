b77fc16e5671aae5152ff557310206b74dc163d9
RustyBamboo <dvolya@gmail.com>
2018-04-27 16:49:47 -0400

    3DSIM: attempt to use screw theory notation to sim dynamics

--- simulation/sub8_3dsim/sim3d.py
+++ simulation/sub8_3dsim/sim3d.py
@@ -5,22 +5,29 @@ import rospy
 import tf.transformations as trns
 from mil_tools import numpy_to_quaternion
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import WrenchStamped
+from geometry_msgs.msg import WrenchStamped, Wrench, Pose, Point, Quaternion, Twist, Vector3
 from mil_ros_tools import rosmsg_to_numpy
+
+def quaternion_multiply(quaternion1, quaternion0):
+    w0, x0, y0, z0 = quaternion0
+    w1, x1, y1, z1 = quaternion1
+    return np.array([-x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0,
+                     x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
+                     -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
+                     x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0], dtype=np.float64)
 
 class Subsim():
     '''
     A simple 2D simulation of the kinematics of NaviGator.
     '''
-    def __init__(self, pose0=np.array([0, 0, 0]), twist0=np.array([0, 0, 0])):
+    def __init__(self):
         # Used to publish current state
         self.odom_publisher = rospy.Publisher("/odom", Odometry, queue_size=1)
 
         # Set initial state from constructor
-        self.pose = np.float64(pose0)
-        self.twist = np.float64(twist0)
-        self.wrench = np.float64([0, 0, 0])
-        # self.thrusts = np.zeros(4, dtype=np.float64)
+        self.pose = Pose(position=Point(0,0,0), orientation=Quaternion(0,0,0,1))
+        self.twist = Twist(linear=Vector3(0,0,0), angular=Vector3(0,0,0))
+        self.wrench = Wrench(force=Vector3(0,0,0), torque=Vector3(0,0,0))
 
         # Get other contants from ROS params
         self.get_params()
@@ -39,14 +46,12 @@ class Subsim():
         rotational_inertia = rospy.get_param('~rotational_inertia')
         self.inertia = np.float64([mass, mass, rotational_inertia])
         self.drag = np.float64(rospy.get_param('~drag'))
-        self.wind = np.float64(rospy.get_param('~wind', [0, 0, 0]))
         self.update_period = rospy.get_param('~update_period', 0.1)
         self.world_frame = rospy.get_param('~world_frame', 'map')
         self.body_frame = rospy.get_param('~body_frame', 'base_link')
-        # self.thrust_map = ThrusterMap.from_ros_params(ns='/thrust_mapper/')
 
     def actual_wrench_cb(self, msg):
-        self.wrench = rosmsg_to_numpy(msg.wrench.force)
+        self.wrench = msg.wrench
 
     def timer_cb(self, timer_event):
         '''
@@ -56,49 +61,38 @@ class Subsim():
         self.publish_odom()
 
     def step(self, dt, wrench):
-        # print(self.drag)
         '''
         Simulate new pose and twist given a time delta and a force/torque applied to NaviGator
         '''
-        s = np.sin(self.pose[2])
-        c = np.cos(self.pose[2])
-        # Rotation Matrix converts body to world by default
-        R = np.array([[c, -s, 0], [s, c, 0], [0, 0, 1]])
-        # wrench = np.array(wrench)
-        posedot, twistdot = self.state_deriv(np.float64(wrench), R)
-        self.pose = self.pose + posedot * dt + 0.5 * R.dot(twistdot) * dt**2
-        self.twist = self.twist + twistdot * dt
+        position = rosmsg_to_numpy(self.pose.position)
+        twist_linear = rosmsg_to_numpy(self.twist.linear)
+        position = position + twist_linear + 0.5 * rosmsg_to_numpy(wrench.force) * dt**2
+        self.pose.position = Point(*position)
 
-    def state_deriv(self, wrench, R):
-        posedot = R.dot(self.twist)
-        twistdot = (1 / self.inertia) * (wrench - self.drag *
-                                         np.sign(self.twist) * self.twist * self.twist + R.T.dot(self.wind))
-        return posedot, twistdot
+        orientation = rosmsg_to_numpy(self.pose.orientation)
+
+        twist_angular = rosmsg_to_numpy(self.twist.angular)
+        twist_angular_as_quat = np.hstack((np.array([0]), twist_angular))
+
+        wrench_torque_as_quat = np.hstack((np.array([0]), rosmsg_to_numpy(wrench.torque)))
+
+        orientation = orientation + 0.5 * quaternion_multiply(orientation, wrench_torque_as_quat) 
+
+        orientation = orientation / np.linalg.norm(orientation)
+        self.pose.orientation = Quaternion(*orientation)
 
     def publish_odom(self):
         '''
         Publish to odometry with latest pose and twist
         '''
-        odom = self.pack_odom(self.pose, self.twist)
+        odom = Odometry()
+        odom.header.stamp = rospy.Time.now()
+        odom.header.frame_id = self.world_frame
+        odom.child_frame_id = self.body_frame
+        odom.pose.pose = self.pose
         self.odom_publisher.publish(odom)
-
-    def pack_odom(self, pose, twist):
-        """
-        Converts pose and twist into an Odometry message
-        """
-        msg = Odometry()
-        msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id = self.world_frame
-        msg.child_frame_id = self.body_frame
-        msg.pose.pose.position.x, msg.pose.pose.position.y = pose[0], pose[1]
-        quat = trns.quaternion_from_euler(0, 0, pose[2])
-        msg.pose.pose.orientation = numpy_to_quaternion(quat)
-        msg.twist.twist.linear.x, msg.twist.twist.linear.y = twist[0:2]
-        msg.twist.twist.angular.z = twist[2]
-        return msg
-
 
 if __name__ == '__main__':
     rospy.init_node('sub8_3Dsim')
-    navsim = Subsim(pose0=np.array([0, 0, np.pi / 2]))
+    subsim = Subsim()
     rospy.spin()
