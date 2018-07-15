65d050afba1ff58eb90f2c06808e6b9e3fccb88d
Lucas Bassett-Audain <lbassettaudain@ufl.edu>
2018-07-15 00:08:00 -0400

    GUESS POINTS: Add sim tested go to points

--- command/sub8_missions/missions/go_to_guess.py
+++ command/sub8_missions/missions/go_to_guess.py
@@ -1,30 +1,32 @@
+#!/usr/bin/env python
+
 from txros import util
 import os
 import sys
-SPEED_LIMIT = .2  # m/s
-fp = open("/home/lucas/clicked.txt", 'r')
-line = fp.readline()
-seq = False
-point_num = sys.argv[1]
-position = [0, 0, 0]
-while line:
-    line = fp.readline().strip()
-    #print line[:6]
-    if line[:6] == ("seq: " + str(point_num)):
-        print "SEQ ", point_num
-        seq = True
-    if seq and line == "position:":
-        print "POSITION"
-        position[0] = float(fp.readline().rstrip()[7:])
-        position[1] = float(fp.readline().rstrip()[7:])
-        position[2] = -1
-        break
-fp.close()
-print position
-if position[0]!=0:
-    print "RUN"
-    @util.cancellableInlineCallbacks
-    def run(sub):
-        yield self.sub.move.look_at_without_pitching(
-            np.array(position[0:3])).go(SPEED_LIMIT)
-        yield self.sub.move.relative(np.array(position[0:3])).go(SPEED_LIMIT)
+import numpy as np
+
+@util.cancellableInlineCallbacks
+def run(sub_singleton):
+    fp = open("/home/sub/clicked.txt", 'r')
+    line = fp.readline()
+    seq = False
+    point_num = 1
+    i = 0
+    position = [0, 0, 0]
+    positions = []
+    while line:
+        line = fp.readline().strip()
+        if line[:5] == ("seq: "):
+            seq = True
+        if seq and line == "position:":
+            position[0] = float(fp.readline().rstrip()[7:])
+            position[1] = float(fp.readline().rstrip()[7:])
+            position[2] = -1
+            positions.append(np.array(position))
+            i = i+1
+    fp.close()
+    for i in xrange(len(positions)):
+        print positions[i][0:3]
+        yield sub_singleton.move.look_at_without_pitching(
+            np.array(positions[i][0:3])).go()
+        yield sub_singleton.move.set_position(np.array(positions[i][0:3])).go()
