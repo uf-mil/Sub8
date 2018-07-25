10bc6e8c3536efd32fd3b2d8daf25d94e716fe03
grymestone <grymestone@ufl.edu>
2018-07-25 00:41:01 -0400

    MISSIONS: Try and fix the myriad of problems

--- perception/sub8_perception/ml_classifiers/path_marker/path_localizer.py
+++ perception/sub8_perception/ml_classifiers/path_marker/path_localizer.py
@@ -5,7 +5,8 @@ import rospy
 import rospkg
 import datetime
 import numpy as np
-
+import os
+import multiprocessing
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
@@ -29,11 +30,11 @@ class classifier(object):
         # Centering Threshold in pixels
         self.centering_thresh = rospy.get_param('~centering_thresh', 50)
         # Color thresholds to find orange, BGR
-        self.lower = rospy.get_param('~lower_color_threshold', [0, 30, 100])
-        self.upper = rospy.get_param('~upper_color_threshold', [100, 255, 255])
+        self.lower = rospy.get_param('~lower_color_threshold', [0, 0, 0])
+        self.upper = rospy.get_param('~upper_color_threshold', [255, 255, 255])
         # Camera topic we are pulling images from for processing
         self.camera_topic = rospy.get_param('~camera_topic',
-                                            '/camera/down/image_rect_color')
+                                            '/camera/front/left/image_rect_color')
         # Number of frames
         self.num_frames = rospy.get_param('~num_frames', 0)
         # Number of objects we detect
@@ -106,10 +107,11 @@ class classifier(object):
         if self.check_timestamp(data):
             return None
         try:
-            # print('Working')
+            print('Working')
             cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
         except CvBridgeError as e:
             print(e)
+
         cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
 
         # Run image through tensorflow graph
@@ -192,7 +194,7 @@ class classifier(object):
             try:
                 # print(output)
                 self.orange_image_pub.publish(
-                    self.bridge.cv2_to_imgmsg(output, 'bgr8'))
+                    self.bridge.cv2_to_imgmsg(np.array(output), 'bgr8'))
             except CvBridgeError as e:
                 print(e)
         return output
@@ -313,6 +315,11 @@ class classifier(object):
         else:
             direction = 'left'
         self.direction_pub.publish(data=direction)
+
+
+
+
+
 
 
 if __name__ == '__main__':
